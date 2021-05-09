9b4cdf8772865b40a30b47d1b62f0ad6707c2eb9
Antonio Aguila <aguilantonio16@gmail.com>
2021-05-09 10:49:40 -0500

    fixing getMessagesFromChat function

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,23 +5,23 @@ import Message from '../models/messageModel.js'
 // @route   GET /api/v1/{from}/messages
 // @access  Private
 const getMessagesFromChat = asyncHandler(async (req, res) => {
-
-    const userId = req.user.id
+    
+    const userId = req.user._id
     const fromUserId = req.params.from
 
     const last30 = await Message.find({
       $or: [
-          { from: userId, para: fromUserId },
-          { from: fromUserId, para: userId },
+          { from: userId, to: fromUserId },
+          { from: fromUserId, to: userId },
       ]
      })
-    .sort({ createdAt: 'asc' })
+    .sort({ createdAt: 'desc' })
     .limit(30);
 
     res.json({ 
       myId: userId,
       idFromUser : fromUserId,
-      last30
+      messages : last30
     })
 })
 
