3f9219f641b8016b7a8f8f54fb87b9105ceb0977
Antonio Aguila <aguilantonio16@gmail.com>
2021-05-09 10:51:55 -0500

    fix error with duplicate messages when you send  a message in your own chat

--- classes/sockets.js
+++ classes/sockets.js
@@ -19,13 +19,13 @@ class Sockets {
             // TODO: validate JWT if not valid disconnect client 
             const [ isValid, id ] = checkJWT( socket.handshake.query['Authorization']  );
             if ( !isValid ) {
-                console.log('unknown socket');
+                //console.log('unknown socket');
                 return socket.disconnect();
             }
             
             
             // TODO: check online status through id 
-            console.log('Client connected', id);
+            //console.log('Client connected', id);
             await userConnected(id);
 
 
@@ -37,13 +37,15 @@ class Sockets {
             socket.on('private-message', async ( payload ) => {
                 const message = await saveMessage( payload );
                 this.io.to( payload.to ).emit('private-message', message);
-                this.io.to( payload.from ).emit('private-message', message);
+                if(payload.from !== payload.to) {
+                    this.io.to( payload.from ).emit('private-message', message);
+                }
             })
 
 
             // TODO: disconnect user and update status in db 
             socket.on('disconnect', async  () => {
-                console.log('client disconnected', id)
+                //console.log('client disconnected', id)
                 await userDisconnected(id);
                 this.io.emit( 'users-list', await getUsers() )
             })
